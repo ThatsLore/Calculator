1a907dbeb0a9ea01b3e915b79d2de0aca33425af
Lorenzo Caminotto <l.caminotto.2640@vallauri.edu>
2023-09-19 13:09:50 +0200

    Dynamic button creation using struct

---
+++ Calculator/FormMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Calculator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(0, 63);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(434, 70);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "0";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 63);
+            this.panel1.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 591);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Calculator/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FormMain : Form
+    {
+
+        public struct BtnStruct
+        {
+            public char Content;
+            public bool IsBold;
+            public BtnStruct(char c, bool b)
+            {
+                this.Content = c;
+                this.IsBold = b;
+            }
+        }
+
+        private BtnStruct[,] buttons =
+        {
+            { new BtnStruct('%', false), new BtnStruct('\u0152', false), new BtnStruct('C', false), new BtnStruct('\u232B', false) }, 
+            { new BtnStruct('\u215F', false), new BtnStruct('\u00B2', false), new BtnStruct('\u221A', false), new BtnStruct('\u00F7', false) }, 
+            { new BtnStruct('7', true), new BtnStruct('8', true), new BtnStruct('9', true), new BtnStruct('\u00D7', false) }, 
+            { new BtnStruct('4', true), new BtnStruct('5', true), new BtnStruct('6', true), new BtnStruct('-', false) }, 
+            { new BtnStruct('1', true), new BtnStruct('2', true), new BtnStruct('3', true), new BtnStruct('+', false) }, 
+            { new BtnStruct('\u00B1', true), new BtnStruct('0', true), new BtnStruct(',', true), new BtnStruct('=', false) },
+        };
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            MakeButtons(buttons.GetLength(0), buttons.GetLength(1));
+        }
+
+        private void MakeButtons(int rows, int cols)
+        {
+            int btnWidth = 80;
+            int btnHeight = 60;
+            int posX = 0;
+            int posY = 116;
+            for(int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Button myButton = new Button();
+                    FontStyle fs = buttons[i, j].IsBold ? FontStyle.Bold : FontStyle.Regular;
+                    myButton.Font = new Font("Segoe UI", 16, fs);
+                    myButton.BackColor = buttons[i, j].IsBold ? Color.White : Color.LightGray;
+                    myButton.Text = buttons[i, j].Content.ToString();
+                    myButton.Width = btnWidth;
+                    myButton.Height = btnHeight;
+                    myButton.Top = posY;
+                    myButton.Left = posX;
+                    this.Controls.Add(myButton);
+                    posX += myButton.Width;
+                }
+                posX = 0;
+                posY += btnHeight;
+            }
+            
+        }
+    }
+}
